3ebac91d31dc0a5fa551e6ed9c1197f269750aa4
Akshay199456 <akshay.kum94@gmail.com>
2020-02-14 23:48:05 -0600

    Connected css static

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-	return 'Hello World!'
+	return render_template('index.html')
 
 @app.route('/expenses')
 def expenses():
-	return 'Expenses page!'
+	return render_template('render.html')
 
 if(__name__ == '__main__'):
 	app.run()
