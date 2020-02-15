940c0ab2d3a46626d3c92add739498da2e106412
Akshay199456 <akshay.kum94@gmail.com>
2020-02-15 02:24:09 -0600

    Created authentication blueprint

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,3 +1,8 @@
+-- In SQLite, data is stored in tables and columns. These need to be created before 
+-- you can store and retrieve data. Flaskr will store users in the user table, 
+-- and posts in the post table. Create a file with the SQL commands needed to create 
+-- empty tables:
+
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
