824094c59a8a967574c01c32fcda9b60aeb80f9e
Akshay199456 <akshay.kum94@gmail.com>
2020-02-16 18:41:50 -0600

    Create category complete

--- flaskr/category.py
+++ flaskr/category.py
@@ -18,11 +18,49 @@ def index():
 		request_type = request.form['submit_button']
 
 		possible_selection = {
-			'create_category' : 'category/create.html',
-			'remove_category' : 'category/remove.html',
-			'add_expenses' : '',
-			'generate_chart': '',
+			'create_category' : 'category.create',
+			'remove_category' : 'category.remove',
+			'add_expenses' : 'category.addExpenses',
+			'generate_chart': 'category.chart',
 		}
-
-		return render_template(possible_selection[request_type])
+		# Need to redirect to proper link on click
+		# return render_template(possible_selection[request_type])
+		return redirect(url_for(possible_selection[request_type]))
 	return render_template('category/index.html')
+
+@bp.route('/create', methods = ('GET', 'POST'))
+@login_required
+def create():
+	if request.method == 'POST':
+		print('Request form: ', request.form)
+		error = None
+		category_type = request.form['category'].title()
+
+		if not category_type:
+			error = 'Category type is required!'
+		
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			existing_category = db.execute(
+				'SELECT c.id, c.type, c.user_id'
+				' FROM category c JOIN user u ON c.user_id = u.id'
+				' WHERE c.type = ? AND c.user_id = ?',(category_type, g.user['id'])
+			).fetchone()
+
+			print('Does category exist: ', existing_category)
+			if existing_category is not None:
+				error = 'Category ' + category_type + ' already exists!'
+				flash(error)
+			else:
+				print('Current user id: ', g.user['id'])
+				db.execute(
+					'INSERT INTO category (type, user_id)'
+					' VALUES (?, ?)', (category_type, g.user['id'])
+				)
+				db.commit()
+				return redirect(url_for('category.index'))
+
+	return render_template('category/create.html')
+
